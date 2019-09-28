d6f3e3c82b5624f4d74380f63e13ab6b5821e4d1
Daniel Malmros <daniel.malmros@creuna.dk>
2019-09-28 19:38:27 +0200

    added GA

--- client/src/main.js
+++ client/src/main.js
@@ -5,9 +5,20 @@ import store from './store'
 import VuePlyr from 'vue-plyr'
 import 'vue-plyr/dist/vue-plyr.css'
 import VuePageTransition from 'vue-page-transition'
+import VueAnalytics from 'vue-analytics'
+
+const isProd = process.env.NODE_ENV === 'production';
 
 Vue.use(VuePlyr)
 Vue.use(VuePageTransition)
+Vue.use(VueAnalytics, {
+  id: "UA-148963191-1",
+  router,
+  debug: {
+    enabled: !isProd,
+    sendHitTask: isProd
+  }
+})
 
 Vue.config.productionTip = false
 
